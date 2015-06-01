414d164c06ef476b85ce7a2a81062fa9e2a24572
Perry Hook <perry.hook@gmail.com>
2015-05-31 17:49:25 -0700

    Slight refactor to output checker.

--- assignment3/output_checker.py
+++ assignment3/output_checker.py
@@ -1,35 +1,34 @@
 import sys
 
 
+def readlines_sorted(file_name):
+    with open(file_name, 'rb') as f:
+        lines = f.readlines()
+    lines.sort()
+    return lines
+
+
+def print_different_lines(lines_1, lines_2):
+    print "output is not the same"
+    count = 0
+    for i in xrange(len(lines_2)):
+        print "{} - {}".format(lines_1[i], lines_2[i])
+        count += 1
+    print "\nleftovers are: "
+    print lines_1[count:]
+
+
 if __name__ == "__main__":
     file_1 = sys.argv[1]
     file_2 = sys.argv[2]
 
-    with open(file_1, 'rb') as f:
-        lines_1 = f.readlines()
-
-    with open(file_2, 'rb') as f:
-        lines_2 = f.readlines()
-
-    lines_1.sort()
-    lines_2.sort()
+    lines_1 = readlines_sorted(file_1)
+    lines_2 = readlines_sorted(file_2)
 
     left = len(lines_1) > len(lines_2)
     if lines_1 == lines_2:
         print "output is the same"
     elif left:
-        print "output is not the same"
-        count = 0
-        for i in xrange(len(lines_2)):
-            print "{} - {}".format(lines_1[i], lines_2[i])
-            count += 1
-        print "\nleftovers from lines_1 are: "
-        print lines_1[count:]
+        print_different_lines(lines_1, lines_2)
     else:
-        print "output is not the same"
-        count = 0
-        for i in xrange(len(lines_1)):
-            print "{} - {}".format(lines_1[i], lines_2[i])
-            count += 1
-        print "\nleftovers from lines_2 are: "
-        print lines_2[count:]
+        print_different_lines(lines_2, lines_1)
